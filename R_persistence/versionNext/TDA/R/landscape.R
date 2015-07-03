7b300290cdc3f4d1cbfdeb304320173096d2852d
Jisu KIM <jisuk1@andrew.cmu.edu>
2015-07-03 20:14:11 +0200

    allow vector input for KK in landscape()

--- R_persistence/versionNext/TDA/R/landscape.R
+++ R_persistence/versionNext/TDA/R/landscape.R
@@ -1,12 +1,23 @@
-landscape<-
-function(Diag, dimension=1, KK=1, tseq=seq( min(Diag[,2:3]), max(Diag[,2:3]) , length=500) )
-{
+landscape <-
+function(Diag, dimension = 1, KK = 1,
+         tseq = seq(min(Diag[, 2:3]), max(Diag[, 2:3]), length = 500)) {
     
-    if (class(Diag)!="diagram" && class(Diag)!="matrix" && !is.data.frame(Diag) )  
-    	stop("Diag should be a diagram, or a P by 3 matrix")
-	if (!is.vector(dimension) || length(dimension)!=1) stop("dimension should be an integer")
-	if (!is.vector(KK) || length(KK)!=1) stop("KK should be an integer")
-	if (!is.vector(tseq) || !is.numeric(tseq)) stop("tseq should be a numeric vector")
+  if (class(Diag) != "diagram" && class(Diag) != "matrix" &&
+      !is.data.frame(Diag)) {
+    stop("Diag should be a diagram, or a P by 3 matrix")
+  }
+  tryCatch(dimension <- as.double(dimension), error = function(e) {
+      stop("dimension should be numeric")})
+  if (length(dimension) != 1 || dimension < 0) {
+    stop("dimension should be an nonnegative integer")
+  }
+  tryCatch(KK <- as.double(KK), error = function(e) {
+      stop("KK should be numeric")})
+  if (min(KK) <= 0) {
+    stop("KK should be positive integer")
+  }
+  tryCatch(tseq <- as.double(tseq), error = function(e) {
+      stop("tseq should be numeric")})
     
     isNA=length(which(Diag[, 1] == dimension))
     if (isNA==0) return(rep(0, length(tseq))) #in case there are no features with dimension "dimension"
@@ -26,5 +37,11 @@ function(Diag, dimension=1, KK=1, tseq=seq( min(Diag[,2:3]), max(Diag[,2:3]) , l
     lambda=sapply(1:length(tseq),  FUN=function(i){
     	sort(fab[i, ], decreasing = TRUE)[KK]  	
     })
+  lambda[is.na(lambda)] <- 0
+  if (length(KK) == 1) {
+    lambda <- matrix(lambda)
+  } else {
+    lambda <- t(lambda)
+  }
     return(lambda)
 }
