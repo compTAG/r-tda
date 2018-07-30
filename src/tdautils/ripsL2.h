b5b3705a94d8c942eff2421400c7ee5e1b080c32
thomashli <thomasli537@gmail.com>
2018-07-30 16:35:08 -0600

    Arbit works to

--- src/tdautils/ripsL2.h
+++ src/tdautils/ripsL2.h
@@ -22,14 +22,19 @@ namespace d = dionysus;
 
 
 //L2 Struct is inside distances.h
-// Feels very janky
+// not sure if I want to typedef here
 
 // typedef     std::vector<double>                                     Point;
 // typedef     std::vector<Point>                                      PointContainer;
 
+// Swapped out
 typedef         d::PairwiseDistances<std::vector<std::vector<double>>, d::L2Distance<std::vector<double>>>           PairDistances;
+
+//Next two lines are fine
 typedef         PairDistances::DistanceType                             DistanceType;
 typedef         PairDistances::IndexType                                VertexR;
+
+
 typedef         Rips< PairDistances, Simplex< VertexR, double > >       Generator;
 typedef         Generator::Simplex                                      SmplxR;
 typedef         Filtration<SmplxR>                                       FltrR;
