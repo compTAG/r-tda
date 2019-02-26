c2a8c7a6452dcf3b2144696b2cc2d9e5639971b9
benholmgren <44175897+benholmgren@users.noreply.github.com>
2019-02-26 12:42:40 -0700

    Now functional roxygen comments
    Roxygen comments have to be outside function, and needed a title to render properly. Added return value as well.

--- R/kde.R
+++ R/kde.R
@@ -1,12 +1,22 @@
+#'@title kde
+#'
+#'@param X an n by d matrix of coordinates used in kernel density estimation
+#'where n is the number of points and d is the dimension
+#'
+#'@param Grid an m by d matrix of coordinates where m is the number of points and d
+#'is the dimension of the base grid referred to in the estimation
+#'
+#'@param h a desired number to be used as the smoothing parameter of the Gaussian Kernel
+#'
+#'@param kertype set by default to to use Gaussian kernel
+#'
+#'@param weight set by default to 1, making all points in X weighted the same unless
+#'otherwise specified
+#'
+#'@return vector of length m (the number of points in the grid) containing the value of
+#'the kernel density estimator for each point in the grid
+
 kde <-
-  #'@param X an n by d matrix of coordinates used in kernel density estimation
-  #'where n is the number of points and d is the dimension
-  #'@param Grid an m by d matrix of coordinates where m is the number of points and d
-  #'is the dimension of the base grid referred to in the estimation
-  #'@param h a desired number to be used as the smoothing parameter of the Gaussian Kernel
-  #'@param kertype set by default to to use Gaussian kernel
-  #'@param weight set by default to 1, making all points in X weighted the same unless
-  #'otherwise specified
 function(X, Grid, h, kertype = "Gaussian", weight = 1, printProgress = FALSE) {
   if (!is.numeric(X) && !is.data.frame(X)) {
     stop("X should be a matrix of coordinates")
